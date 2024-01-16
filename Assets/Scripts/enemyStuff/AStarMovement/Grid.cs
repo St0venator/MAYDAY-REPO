7ba6f161bff1401b425667034718083f7610d8bc
SpykSpartan <107502836+SpykSpartan@users.noreply.github.com>
2024-01-15 20:07:32 -0500

    Reworking Astar

--- Assets/Scripts/enemyStuff/AStarMovement/Grid.cs
+++ Assets/Scripts/enemyStuff/AStarMovement/Grid.cs
@@ -2,104 +2,89 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Grid : MonoBehaviour 
-{
+public class Grid : MonoBehaviour {
+
 	public bool displayGridGizmos;
 	public LayerMask unwalkableMask;
-	public Vector3 gridWorldSize;
+	public Vector2 gridWorldSize;
 	public float nodeRadius;
-	public TerrianType[] walkableRegions;
+	public TerrainType[] walkableRegions;
 	public int obstacleProximityPenalty = 10;
+	Dictionary<int,int> walkableRegionsDictionary = new Dictionary<int, int>();
 	LayerMask walkableMask;
-	Dictionary<int, int> walkableRegionDictionary = new Dictionary<int, int>();
-	
-	Node[,,] grid;
+
+	Node[,] grid;
 
 	float nodeDiameter;
-	int gridSizeX, gridSizeY, gridSizeZ;
-	
+	int gridSizeX, gridSizeY;
+
 	int penaltyMin = int.MaxValue;
 	int penaltyMax = int.MinValue;
 
-	void Awake() 
-	{
+	void Awake() {
 		nodeDiameter = nodeRadius*2;
 		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
 		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
-		gridSizeZ = Mathf.RoundToInt(gridWorldSize.z/nodeDiameter);
 
-		foreach(TerrianType region in walkableRegions)
-		{
+		foreach (TerrainType region in walkableRegions) {
 			walkableMask.value |= region.terrainMask.value;
-			walkableRegionDictionary.Add((int)Mathf.Log(region.terrainMask.value, 1), region.terrainPenatly);
+			walkableRegionsDictionary.Add((int)Mathf.Log(region.terrainMask.value,2),region.terrainPenalty);
 		}
 
 		CreateGrid();
 	}
 
-	public int MaxSize
-	{
-		get
-		{
-			return gridSizeX * gridSizeY * gridSizeZ;
+	public int MaxSize {
+		get {
+			return gridSizeX * gridSizeY;
 		}
 	}
 
-	void CreateGrid() 
-	{
-		grid = new Node[gridSizeX,gridSizeY, gridSizeZ];
-		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2 - Vector3.up * gridWorldSize.z/2;
+	void CreateGrid() {
+		grid = new Node[gridSizeX,gridSizeY];
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
 
-		for (int x = 0; x < gridSizeX; x ++) 
-		{
-			for (int y = 0; y < gridSizeY; y ++) 
-			{
-				for (int z = 0; z < gridSizeZ; z ++)
-				{
-					Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius) + Vector3.up * (z * nodeDiameter + nodeRadius);
-					bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
+		for (int x = 0; x < gridSizeX; x ++) {
+			for (int y = 0; y < gridSizeY; y ++) {
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+				bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
 
-					int movementPenalty = 0;
+				int movementPenalty = 0;
 
-					
-					Ray ray = new Ray(worldPoint + Vector3.left * 50, Vector3.right);
-					RaycastHit hit;
-					if(Physics.Raycast(ray, out hit, 100, walkableMask))
-					{
-						walkableRegionDictionary.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
-					}
 
-					if(!walkable)
-					{
-						movementPenalty += obstacleProximityPenalty;
-					}
-					
-
-					grid[x,y,z] = new Node(walkable,worldPoint, x,y,z, movementPenalty);
+				Ray ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
+				RaycastHit hit;
+				if (Physics.Raycast(ray,out hit, 100, walkableMask)) {
+					walkableRegionsDictionary.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
 				}
+
+				if (!walkable) {
+					movementPenalty += obstacleProximityPenalty;
+				}
+
+
+				grid[x,y] = new Node(walkable,worldPoint, x,y, movementPenalty);
 			}
 		}
-		//BlurPenaltyMap(3);
+
+		BlurPenaltyMap (3);
+
 	}
 
-	/*void BlurPenaltyMap(int blurSize)
-	{
+	void BlurPenaltyMap(int blurSize) {
 		int kernelSize = blurSize * 2 + 1;
 		int kernelExtents = (kernelSize - 1) / 2;
 
 		int[,] penaltiesHorizontalPass = new int[gridSizeX,gridSizeY];
 		int[,] penaltiesVerticalPass = new int[gridSizeX,gridSizeY];
 
-		for (int y = 0; y < gridSizeY; y++) 
-		{
-			for (int x = -kernelExtents; x <= kernelExtents; x++) 
-			{
+		for (int y = 0; y < gridSizeY; y++) {
+			for (int x = -kernelExtents; x <= kernelExtents; x++) {
 				int sampleX = Mathf.Clamp (x, 0, kernelExtents);
 				penaltiesHorizontalPass [0, y] += grid [sampleX, y].movementPenalty;
 			}
 
-			for (int x = 1; x < gridSizeX; x++) 
-			{
+			for (int x = 1; x < gridSizeX; x++) {
 				int removeIndex = Mathf.Clamp(x - kernelExtents - 1, 0, gridSizeX);
 				int addIndex = Mathf.Clamp(x + kernelExtents, 0, gridSizeX-1);
 
@@ -107,10 +92,8 @@ public class Grid : MonoBehaviour
 			}
 		}
 			
-		for (int x = 0; x < gridSizeX; x++) 
-		{
-			for (int y = -kernelExtents; y <= kernelExtents; y++) 
-			{
+		for (int x = 0; x < gridSizeX; x++) {
+			for (int y = -kernelExtents; y <= kernelExtents; y++) {
 				int sampleY = Mathf.Clamp (y, 0, kernelExtents);
 				penaltiesVerticalPass [x, 0] += penaltiesHorizontalPass [x, sampleY];
 			}
@@ -118,8 +101,7 @@ public class Grid : MonoBehaviour
 			int blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass [x, 0] / (kernelSize * kernelSize));
 			grid [x, 0].movementPenalty = blurredPenalty;
 
-			for (int y = 1; y < gridSizeY; y++) 
-			{
+			for (int y = 1; y < gridSizeY; y++) {
 				int removeIndex = Mathf.Clamp(y - kernelExtents - 1, 0, gridSizeY);
 				int addIndex = Mathf.Clamp(y + kernelExtents, 0, gridSizeY-1);
 
@@ -127,80 +109,66 @@ public class Grid : MonoBehaviour
 				blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass [x, y] / (kernelSize * kernelSize));
 				grid [x, y].movementPenalty = blurredPenalty;
 
-				if (blurredPenalty > penaltyMax) 
-				{
+				if (blurredPenalty > penaltyMax) {
 					penaltyMax = blurredPenalty;
 				}
-				if (blurredPenalty < penaltyMin) 
-				{
+				if (blurredPenalty < penaltyMin) {
 					penaltyMin = blurredPenalty;
 				}
 			}
 		}
 
-	}*/
+	}
 
 	public List<Node> GetNeighbours(Node node) {
 		List<Node> neighbours = new List<Node>();
 
-		for (int x = -1; x <= 1; x++) 
-		{
-			for (int y = -1; y <= 1; y++) 
-			{
-				for(int z = -1; z <= 1; z++)
-				{
-					if (x == 0 && y == 0)
-						continue;
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				if (x == 0 && y == 0)
+					continue;
 
-					int checkX = node.gridX + x;
-					int checkY = node.gridY + y;
-					int checkZ = node.gridZ + z;
+				int checkX = node.gridX + x;
+				int checkY = node.gridY + y;
 
-					if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY && checkZ >= 0 && checkZ < gridSizeZ) 
-					{
-						neighbours.Add(grid[checkX,checkY,checkZ]);
-					}
+				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
+					neighbours.Add(grid[checkX,checkY]);
 				}
 			}
 		}
+
 		return neighbours;
 	}
-	
 
-	public Node NodeFromWorldPoint(Vector3 worldPosition) 
-	{
+
+	public Node NodeFromWorldPoint(Vector3 worldPosition) {
 		float percentX = (worldPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
-		float percentY = (worldPosition.y + gridWorldSize.y/2) / gridWorldSize.y;
-		float percentZ = (worldPosition.z + gridWorldSize.z/2) / gridWorldSize.z;
+		float percentY = (worldPosition.z + gridWorldSize.y/2) / gridWorldSize.y;
 		percentX = Mathf.Clamp01(percentX);
 		percentY = Mathf.Clamp01(percentY);
-		percentZ = Mathf.Clamp01(percentZ);
 
 		int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
 		int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
-		int z = Mathf.RoundToInt((gridSizeZ-1) * percentZ);
-		return grid[x,y,z];
+		return grid[x,y];
 	}
 
-	public List<Node> path;
-	void OnDrawGizmos() 
-	{
-		Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x, gridWorldSize.z, gridWorldSize.y));
-		if (grid != null && displayGridGizmos) 
-		{
-			foreach (Node n in grid) 
-			{
-				Gizmos.color = (n.walkable)?Color.white:Color.red;
-				Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
+		if (grid != null && displayGridGizmos) {
+			foreach (Node n in grid) {
+
+				Gizmos.color = Color.Lerp (Color.white, Color.black, Mathf.InverseLerp (penaltyMin, penaltyMax, n.movementPenalty));
+				Gizmos.color = (n.walkable)?Gizmos.color:Color.red;
+				Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter));
 			}
 		}
 	}
-}
 
-[System.Serializable]
-public class TerrianType
-{
-	public LayerMask terrainMask;
-	public int terrainPenatly;
-}
+	[System.Serializable]
+	public class TerrainType {
+		public LayerMask terrainMask;
+		public int terrainPenalty;
+	}
 
+
+}
--- Assets/Scripts/enemyStuff/AStarMovement/Unit.cs
+++ Assets/Scripts/enemyStuff/AStarMovement/Unit.cs
@@ -1,109 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class Unit : MonoBehaviour 
-{
-
-	const float minPathUpdateTime = .2f;
-	const float pathUpdateMoveThreshold = .5f;
+public class Unit : MonoBehaviour {
 
 	public Transform target;
 	public float speed = 20;
-	public float turnSpeed = 3;
 	public float turnDst = 5;
-	public float stoppingDst = 10;
 
 	Path path;
 
-	void Start() 
-    {
-		StartCoroutine (UpdatePath ());
+	void Start() {
+		PathRequestManager.RequestPath(transform.position,target.position, OnPathFound);
 	}
 
-	public void OnPathFound(Vector3[] waypoints, bool pathSuccessful) 
-    {
-		if (pathSuccessful) 
-        {
-			path = new Path(waypoints, transform.position, turnDst, stoppingDst);
+	public void OnPathFound(Vector3[] waypoints, bool pathSuccessful) {
+		if (pathSuccessful) {
+			path = new Path(waypoints, transform.position, turnDst);
 
 			StopCoroutine("FollowPath");
 			StartCoroutine("FollowPath");
 		}
 	}
 
-	IEnumerator UpdatePath() {
+	IEnumerator FollowPath() {
 
-
-		if (Time.timeSinceLevelLoad < .3f) 
-        {
-			yield return new WaitForSeconds (.3f);
-		}
-		PathRequestManager.RequestPath (transform.position, target.position, OnPathFound);
-
-		float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
-		Vector3 targetPosOld = target.position;
-
-		while (true) 
-        {
-			yield return new WaitForSeconds (minPathUpdateTime);
-			if ((target.position - targetPosOld).sqrMagnitude > sqrMoveThreshold) 
-            {
-				PathRequestManager.RequestPath (transform.position, target.position, OnPathFound);
-				targetPosOld = target.position;
-			}
-		}
-	}
-
-	IEnumerator FollowPath() 
-    {
-		bool followingPath = true;
-		int pathIndex = 0;
-		transform.LookAt (path.lookPoints [0]);
-
-		float speedPercent = 1;
-
-		while (followingPath) {
-			Vector2 pos2D = new Vector2 (transform.position.x, transform.position.z);
-			while (path.turnBoundaries [pathIndex].HasCrossedLine (pos2D)) 
-            {
-				if (pathIndex == path.finishLineIndex)
-                {
-					followingPath = false;
-					break;
-				} 
-                else 
-                {
-					pathIndex++;
-				}
-			}
-
-			if (followingPath) 
-            {
-				if (pathIndex >= path.slowDownIndex && stoppingDst > 0) 
-                {
-					speedPercent = Mathf.Clamp01 (path.turnBoundaries [path.finishLineIndex].DistanceFromPoint (pos2D) / stoppingDst);
-					if (speedPercent < 0.01f) 
-                    {
-						followingPath = false;
-					}
-				}
-
-				Quaternion targetRotation = Quaternion.LookRotation (path.lookPoints [pathIndex] - transform.position);
-				transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-				transform.Translate (Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
-			}
-
+		while (true) {
 			yield return null;
 
 		}
 	}
 
-	public void OnDrawGizmos()
-    {
-		if (path != null) 
-        {
-			path.DrawWithGizmos ();
+	public void OnDrawGizmos() {
+		if (path != null) {
+			path.DrawWithGizmos();
 		}
 	}
 }
