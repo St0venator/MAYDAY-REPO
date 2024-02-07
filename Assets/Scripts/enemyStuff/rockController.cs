fd550a5636d1ec80f040b8cf490204cb86f17b22
St0venator <104474628+St0venator@users.noreply.github.com>
2024-02-07 18:00:21 -0500

    Boulder Changes

--- Assets/Scripts/enemyStuff/rockController.cs
+++ Assets/Scripts/enemyStuff/rockController.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class rockController : MonoBehaviour
 {
-    Rigidbody rb;
+    public LayerMask mask;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb.drag = Random.Range(0f, 0.2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray zRay = new Ray(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(0, 0, 1));
+
+        if (Physics.Raycast(zRay, out RaycastHit zHit, float.MaxValue, mask))
+        {
+            
+            transform.position = new Vector3(transform.position.x, transform.position.y, zHit.point.z - 5);
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector3(0, 0, 5), ForceMode.Acceleration);
+        
     }
 }
