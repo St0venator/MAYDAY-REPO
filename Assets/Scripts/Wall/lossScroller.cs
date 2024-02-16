b6ca97ac921132e3d6bcbced67d99b7e8b16f79a
St0venator <104474628+St0venator@users.noreply.github.com>
2024-02-16 12:55:55 -0500

    finishing score system

--- Assets/Scripts/Wall/lossScroller.cs
+++ Assets/Scripts/Wall/lossScroller.cs
@@ -6,20 +6,23 @@ using UnityEngine;
 public class lossScroller : MonoBehaviour
 {
     public float spd;
+    public Vector3 direction = Vector3.up;
+    public Vector3 naturalPos = new Vector3(0, 0, 130);
+    public float yTarget = 550;
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = direction.normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up * spd * Time.deltaTime;
+        transform.position += direction * spd * Time.deltaTime;
 
-        if(transform.position.y >= 550)
+        if(transform.position.y - yTarget <= 0.5f)
         {
-            transform.position = new Vector3(0, 0, 130);
+            transform.position = naturalPos;
         }
     }
 }
