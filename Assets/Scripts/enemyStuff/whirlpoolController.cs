b7a58b7c00a36a3ea3c2b2745ca3de6fc6589783
St0venator <104474628+St0venator@users.noreply.github.com>
2024-02-18 20:38:05 -0500

    whirlpooooooolll

---
+++ Assets/Scripts/enemyStuff/whirlpoolController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class whirlpoolController : MonoBehaviour
+{
+    Vector3 targetScale;
+    Vector3 originScale;
+
+    public float growthScale = 1f;
+    float growthPos = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originScale = transform.localScale;
+
+        float randScale = Random.Range(1.1f, 1.3f);
+        targetScale = transform.localScale * randScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.Lerp(originScale, targetScale, growthPos * growthScale);
+
+        growthPos += Time.deltaTime;
+    }
+}
