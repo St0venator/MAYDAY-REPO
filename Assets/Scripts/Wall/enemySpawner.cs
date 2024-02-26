b0f54ccd33b50d8571be16bf112e8c4bb560234a
St0venator <104474628+St0venator@users.noreply.github.com>
2024-02-26 12:27:45 -0500

    backup commit

--- Assets/Scripts/Wall/enemySpawner.cs
+++ Assets/Scripts/Wall/enemySpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemySpawner : MonoBehaviour
 {
@@ -9,15 +10,44 @@ public class enemySpawner : MonoBehaviour
     [SerializeField] float zPos;
     [SerializeField] GameObject anchor;
     [SerializeField] GameObject whirlpool;
+    [SerializeField] GameObject mine;
+    [SerializeField] GameObject avalanche;
+    [SerializeField] LayerMask mask;
     // Start is called before the first frame update
     void Start()
     {
-        spawnWhirlpool();
+        spawnMine(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void spawnMine()
+    {
+        bool validSpawn = false;
+        float xRange;
+        float yRange;
+
+        do
+        {
+            xRange = Random.Range(-100, 100);
+            yRange = Random.Range(0, 200);
+
+            Ray zRay = new Ray(new Vector3(xRange, yRange), new Vector3(0, 0, 1));
+
+            if (Physics.Raycast(zRay, out RaycastHit zHit, float.MaxValue, mask))
+            {
+                validSpawn = true;
+            }
+        } while(!validSpawn);
+
+        Vector3 spawnPos = new Vector3(xRange, yRange, zPos);
+
+        Instantiate(mine, spawnPos, Quaternion.identity);
+
         
     }
 
@@ -40,5 +70,14 @@ public class enemySpawner : MonoBehaviour
         GameObject newAnchor = Instantiate(anchor, spawnPos, Quaternion.identity);
 
         newAnchor.transform.Rotate(new Vector3(-90, 0, 0));
+    }
+
+    void spawnAvalanche()
+    {
+        Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        Vector3 spawnPos = new Vector3(playerPos.x, yPos, zPos);
+
+        Instantiate(avalanche, spawnPos, Quaternion.identity);
     }
 }
