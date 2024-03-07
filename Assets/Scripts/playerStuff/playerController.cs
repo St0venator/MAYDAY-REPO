22c4188d09310125b80283c8a526d5a737ff47f4
St0venator <104474628+St0venator@users.noreply.github.com>
2024-03-06 20:10:27 -0500

    Fixed

--- Assets/Scripts/playerStuff/playerController.cs
+++ Assets/Scripts/playerStuff/playerController.cs
@@ -56,13 +56,13 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isStunned)
+        if (isStunned)
         {
             if (stunTimer <= 0)
             {
                 isStunned = false;
                 stunTimer = 3.0f;
-                
+
             }
             else
             {
@@ -70,6 +70,9 @@ public class playerController : MonoBehaviour
                 Debug.Log(stunTimer);
             }
             Debug.Log(isStunned);
+
+        }
+
         OXY.oxygenDecrement(Time.deltaTime);
         oxygenText.text = "Oxygen: " + OXY.displayOxygen();
 
@@ -85,23 +88,6 @@ public class playerController : MonoBehaviour
             isSlash = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftControl) && canJump)
-        {
-            GameObject bullet = Instantiate(bulletRef, transform.position, Quaternion.identity);
-            bullet.transform.position -= new Vector3(0, 0, 10);
-            bullet.GetComponent<bulletController>().velocity = (worldCursor.transform.position - transform.position).normalized * 10;
-            SoundManager.PlayShootSFX();
-        }
-        else
-        {
-
-            StopAllCoroutines();
-            if(isSlash){
-                    SoundManager.PlaySwordSFX();//Plays sword SFX when moving                  
-            }
-            else{
-                SoundManager.PlayJumpSound();//Plays jump SFX when moving
-            }
 
             if (Input.GetKeyDown(KeyCode.LeftControl) && canJump)
             {
@@ -109,10 +95,12 @@ public class playerController : MonoBehaviour
                 bullet.transform.position -= new Vector3(0, 0, 10);
                 bullet.GetComponent<bulletController>().velocity = (worldCursor.transform.position - transform.position).normalized * 10;
                 SoundManager.PlayShootSFX();
+            /*
                 anim.SetBool("Jumped", true);
                 anim.SetBool("IsMidair", true);
                 anim.SetBool("IsGrounded", false);
                 StartCoroutine(climb(worldCursor.transform.position, climbSpeed));
+            */
             }
 
             //If the player hits Left Shift, stop all current climb coroutines, and start a new one targeting the node the cursor is selecting
@@ -141,7 +129,6 @@ public class playerController : MonoBehaviour
                 anim.SetBool("IsGrounded", false);
                 StartCoroutine(fall(worldCursor.transform.position, climbSpeed));
             }
-        }
     }
 
     private void OnTriggerEnter(Collider other)
